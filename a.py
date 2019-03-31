877537a002c32c88fa2ad389c14cc8fcc54471e9
gonzales12595 <timothypaul.gonzales@uap.asia>
2019-03-31 16:28:17 +1100

    added age stack bar for task 1

--- a.py
+++ a.py
@@ -26,18 +26,20 @@ def age_scattered(df):
 def binning(df,col,num_bins,labels=[]):
     '''
     binning for age using histogram analysis and creates a new column with bin labels
+
+    optional labels parameter - if there is a defined definition for the label
     '''
     print(len(labels))
     hist,bin_edges = np.histogram(df[col],num_bins)
-    #print(hist)
-    #print(bin_edges)
     #filter
     start = bin_edges[0]
     bins = list()
     plt.figure(figsize=(10,7))
-    #add bin column
+    
+    #add bin to column
     df[str(col)+'_bin'] = None
     for i in range(1,len(bin_edges)):
+        
         #labelling
         if len(labels) != num_bins:
             if bin_edges[-1] == bin_edges[i]:
@@ -52,12 +54,54 @@ def binning(df,col,num_bins,labels=[]):
         start = round(bin_edges[i])
     return df
 
+
+def age_stacked_bar(chosen_col,df):
+    '''
+    Function to create stacked bar chart
+    '''
+    #predefined colour scheme. Note: must be more than number of column values
+    colour = ['blue','pink','brown','red','orange']
+    
+    #create bar chart
+    for col in chosen_col:
+        if col != 'age' and col != 'sex' and col != 'bin':
+            
+            #get unique age_bin values
+            age_bins = df.age_bin.unique()
+            age_bins.sort()
+
+            #format table for bar chart
+            df1 = df[[col,'age_bin','age']]
+            tots = df1.groupby(['age_bin',col]).count()
+            tots = tots.unstack(level=1)
+            tots.columns = tots.columns.droplevel(level=0)
+            tots = tots.fillna(0)
+
+            #get unique column values
+            col_vals = df[col].unique()
+            col_vals.sort()
+            
+            #iterate through unique col values to create stack bar chart
+            bottom_value =0
+            for i in range(len(col_vals)):
+                plt.bar(age_bins, tots[col_vals[i]],bottom=bottom_value, color=colour[i],label=col_vals[i])
+                #update start value of the next stack
+                bottom_value += tots[col_vals[i]]
+            
+            plt.legend()
+            plt.xlabel('Age')
+            plt.ylabel(col.replace("_"," ").title())
+            plt.show()
+  
+                
+
+chosen_col = ['chest_pain','electrocardiographic_result']#,  'electrocardiographic_result', 'major_vessels', 'thal', 'target']
+
 df = get_data("processed.cleveland.data")
 #example bin size is 8
 labels=['a','b','c','d','e','f','g','h']
-df = binning(df,'age',8,labels)
-print(df.head())
-
+df = binning(df,'age',8)
+age_stacked_bar(chosen_col,df)
 #age_scattered(df)
 
 
