707ff1656402c4d4bc73aac7e5ddd69516952acd
amriteshs <48743872+amriteshs@users.noreply.github.com>
2019-04-11 14:49:47 +1000

    updated algo to random forest classifier

--- task02.py
+++ task02.py
@@ -1,13 +1,7 @@
 import pandas as pd
-# from sklearn.feature_selection import RFE, SelectFromModel
-# from sklearn.linear_model import LogisticRegression
-# from sklearn.model_selection import train_test_split
-# from sklearn.pipeline import Pipeline
-# from sklearn.preprocessing import StandardScaler
-# from sklearn.decomposition import PCA
-# from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import RandomForestRegressor
-# from sklearn.svm import LinearSVC
+# import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import RFE
 
 
 def get_data(filename):
@@ -20,59 +14,35 @@ def get_data(filename):
 
 def data_cleansing(dataframe):
     dataframe = dataframe[dataframe.major_vessels != '?']
+    dataframe = dataframe[dataframe.thal != '?']
 
-    return dataframe[dataframe.thal != '?']
+    return dataframe
 
 
-if __name__ == '__main__':
+def feature_importance():
+    # create dataframe from csv file and clean the data
     df = data_cleansing(get_data('processed.cleveland.data'))
     df.major_vessels = pd.to_numeric(df.major_vessels)
     df.thal = pd.to_numeric(df.thal)
 
-    for row in df.itertuples():
-        print(row)
+    # list of attributes apart from the target
+    x = df.drop('target', axis=1)
 
-    X = df.drop('target', axis=1)
+    # target attribute
     y = df.target
 
-    # RANDOM FOREST REGRESSOR
+    # fit random forest classifier
+    clf = RandomForestClassifier(n_estimators=100)
+    clf.fit(x, y)
 
-    rf = RandomForestRegressor(n_estimators=1000)
-    rf.fit(X, y)
+    # sort features based on their relative importance to the target
+    importance_scores = sorted(zip(x.columns, clf.feature_importances_), key=lambda k: k[1], reverse=True)
 
-    for feature in sorted(zip(X.columns, rf.feature_importances_), key=lambda x: x[1], reverse=True):
-        print(feature)
+    return importance_scores
 
-    # # RANDOM FOREST CLASSIFIER
-    #
-    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-    #
-    # clf = RandomForestClassifier(n_estimators=10000, random_state=0, n_jobs=-1)
-    # clf.fit(X_train, y_train)
-    #
-    # for feature in sorted(zip(X.columns, clf.feature_importances_), key=lambda x: x[1], reverse=True):
-    #     print(feature)
 
-    # # PRINCIPAL COMPONENT ANALYSIS
-    #
-    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-    #
-    # sc = StandardScaler()
-    # X_train = sc.fit_transform(X_train)
-    # X_test = sc.fit_transform(X_test)
-    #
-    # pca = PCA()
-    # X_train = pca.fit_transform(X_train)
-    # X_test = pca.fit_transform(X_test)
-    #
-    # explained_variance = pca.explained_variance_ratio_
-    # print(explained_variance)
+if __name__ == '__main__':
+    feature_scores = feature_importance()
 
-    # # RECURSIVE FEATURE ELIMINATION
-    #
-    # model = LogisticRegression(solver='liblinear', multi_class='auto')
-    # rfe = RFE(model, 1)
-    #
-    # rfe = rfe.fit(X, df.target)
-    # print(rfe.support_)
-    # print(rfe.ranking_)
+    for row in feature_scores:
+        print(row)
