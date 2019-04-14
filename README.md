7c8a382b18b6c288606c0f4d18c529093cca1f9b
amriteshs <48743872+amriteshs@users.noreply.github.com>
2019-04-14 22:37:32 +1000

    updated README

--- README.md
+++ README.md
@@ -47,7 +47,7 @@ For Linux and Mac:
 ```
 export FLASK_APP=flaskr
 export FLASK_ENV=development
-python3 -m flask run
+python -m flask run
 
 ```
 
--- start.py
+++ start.py
@@ -4,4 +4,4 @@ import os
 if platform.system() == 'Windows':
     os.system("set FLASK_APP=flaskr & set FLASK_ENV=development & flask run")
 else:
-    os.system("export FLASK_APP=flaskr & export FLASK_ENV=development & python3 -m flask run")
+    os.system("export FLASK_APP=flaskr & export FLASK_ENV=development & python -m flask run")
